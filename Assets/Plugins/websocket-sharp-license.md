f24601e67e2607416c6b6396278abab934843376
YusukeKato <s1526035cn@s.chibakoudai.jp>
2019-04-24 21:06:10 +0900

    Unity Project

---
+++ Assets/Plugins/websocket-sharp-license.md
@@ -0,0 +1,5 @@
+# websocket-sharp
+Copyright (c) 2010-2019 sta.blockhead
+Released under the MIT license
+
+https://github.com/sta/websocket-sharp/blob/master/LICENSE.txt
---
+++ Assets/Scripts/CustomTrackableEventHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class CustomTrackableEventHandler : DefaultTrackableEventHandler
+{
+    public bool isFound = false;
+    public GameObject ImageTarget;
+    public GameObject ParentObject;
+    public GameObject MarkerPrefab;
+    public GameObject rosConnector;
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+
+        bool isStart = false;
+        isStart = rosConnector.GetComponent<ROSConnector>().isStart;
+        if (isStart == true && isFound == false)
+        {
+            isFound = true;
+            ImageTarget.transform.parent = ParentObject.transform;
+            ParentObject.transform.position = ImageTarget.transform.position;
+            ParentObject.transform.rotation = ImageTarget.transform.rotation;
+        }
+    }
+}
